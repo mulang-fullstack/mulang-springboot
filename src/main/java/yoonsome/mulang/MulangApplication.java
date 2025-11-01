01332c4930a27723d79f2c303418a1b41c922eea
jhy <idpc8090@gmail.com>
2025-11-01 18:06:18 +0900

    애플리케이션 프로퍼티 주요 부분 환경변수 처리

--- src/main/java/yoonsome/mulang/MulangApplication.java
+++ src/main/java/yoonsome/mulang/MulangApplication.java
@@ -2,12 +2,19 @@ package yoonsome.mulang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class MulangApplication {
+public class MulangApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MulangApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MulangApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MulangApplication.class, args);
+    }
 
 }
